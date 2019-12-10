2c26df0e8f85d336419ddbc6b1e1da7197ab444c
Alex Petitjean <alex.m.petitjean@mobiledoorman.com>
2019-12-10 10:06:08 -0600

    Configure Spek test framework

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,27 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven(url = "https://dl.bintray.com/spekframework/spek-dev/")
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+
+    testRuntimeOnly(kotlin("reflect"))
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit"))
+
+    // Spek
+
+    val spekVersion = "2.0.9"
+    testImplementation("org.spekframework.spek2:spek-dsl-jvm:$spekVersion") {
+        exclude(group = "org.jetbrains.kotlin")
+    }
+    testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:$spekVersion") {
+        exclude(group = "org.junit.platform")
+        exclude(group = "org.jetbrains.kotlin")
+    }
+    testImplementation(group = "org.junit.platform", name = "junit-platform-engine", version = "1.3.0-RC1")
 }
 
 tasks {
@@ -22,5 +39,11 @@ tasks {
 
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+
+    withType<Test> {
+        useJUnitPlatform {
+            includeEngines("spek2")
+        }
     }
 }
